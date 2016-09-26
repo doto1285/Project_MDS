ab4d50f5a3e3ec13fcd9307ae0d2d27740a83220
notebook <lmw313@naver.com>
2016-09-26 10:17:31 +0900

    이민우 SPA_MainController 오류 해결 커밋

--- src/main/java/kr/ac/sungkyul/MDS/controller/SPA_MainController.java
+++ src/main/java/kr/ac/sungkyul/MDS/controller/SPA_MainController.java
@@ -19,41 +19,41 @@ import kr.ac.sungkyul.MDS.vo.MemberVo;
 @Controller
 @RequestMapping("/SPA")
 public class SPA_MainController {
-	
-	@Autowired
-	MemberService memberService;
-	
-	@Autowired
-	SPA_CategoryListService categoryListService;
-	
-	
-	@RequestMapping(value = "{domain}/main", method = RequestMethod.GET)
-	public String index(@PathVariable String domain, HttpSession session) {
-		//세션 값이랑 userid랑 일치하는지 확인
-//		if(!isUserCheck(userid, session)) {
-//			return "redirect:/main";	
-//		}
-		
-		return "SPA/main/index";
-	}
-	
-	@RequestMapping(value = "{domain}/category", method = RequestMethod.GET)
-	public String categorylist(@PathVariable String domain, HttpSession session, Model model) {
-		//세션 값이랑 userid랑 일치하는지 확인
-//		if(!isUserCheck(userid, session)) {
-//			return "redirect:/main";	
-//		}
-		
-		List<CategoryListVo> categoryListVo = categoryListService.getCategoryList(domain);
-		model.addAttribute("categoryListVo", categoryListVo);
-		return "SPA/category/categorylist";
-	}
-	
-	public boolean isUserCheck(String domain, HttpSession session) {
-		MemberVo memberVo = (MemberVo) session.getAttribute("member");
-		if( memberVo == null || memberVo.getMember_id() != domain) {
-			return false;	
-		}
-		return true;		
-	}
+   
+   @Autowired
+   MemberService memberService;
+   
+   @Autowired
+   SPA_CategoryListService categoryListService;
+   
+   
+   @RequestMapping(value = "{domain}/main", method = RequestMethod.GET)
+   public String index(@PathVariable String domain, HttpSession session) {
+      //세션 값이랑 userid랑 일치하는지 확인
+//      if(!isUserCheck(userid, session)) {
+//         return "redirect:/main";   
+//      }
+      
+      return "SPA/main/index";
+   }
+   
+   @RequestMapping(value = "{domain}/category", method = RequestMethod.GET)
+   public String categorylist(@PathVariable String domain, HttpSession session, Model model) {
+      //세션 값이랑 userid랑 일치하는지 확인
+//      if(!isUserCheck(userid, session)) {
+//         return "redirect:/main";   
+//      }
+      
+      List<CategoryListVo> categoryListVo = categoryListService.getCategoryList(domain);
+      model.addAttribute("categoryListVo", categoryListVo);
+      return "SPA/category/categorylist";
+   }
+   
+   public boolean isUserCheck(String domain, HttpSession session) {
+      MemberVo memberVo = (MemberVo) session.getAttribute("member");
+      if( memberVo == null || memberVo.getMember_id() != domain) {
+         return false;   
+      }
+      return true;      
+   }
 }
